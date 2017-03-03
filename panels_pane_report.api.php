58898fca3196e0b7e2981a7d629830f97c886b33
davidstinemetze <davidstinemetze@2508346.no-reply.drupal.org>
2017-03-03 14:51:32 -0600

    Issue #2856866 by WidgetsBurritos, bighappyface: Identify and implement alter hooks

---
+++ panels_pane_report.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Hooks related to panels panel report.
+ */
+
+/**
+ * Modifies a Panels Pane Report item array right before added to the database.
+ *
+ * @param array $item_array
+ *   Item array element.
+ */
+function hook_panels_pane_report_report_item_alter(&$item_array) {
+  $item_array['some_field'] = 'some_value';
+}
